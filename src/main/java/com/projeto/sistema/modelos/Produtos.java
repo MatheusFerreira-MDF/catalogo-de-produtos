78f8ce8efa1b2cf6e328b9d2e6854df1d831c44c
MatheusFerreira-MDF <theusferreiraode@icloud.com>
2024-10-14 20:28:00 -0300

    Add files via upload

---
+++ src/main/java/com/projeto/sistema/modelos/Produtos.java
@@ -0,0 +1,34 @@
+package com.projeto.sistema.modelos;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Produtos")
+public class Produtos implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
