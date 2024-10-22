efff296e956b7dc8467e24c0b8d24fa39466640b
MatheusFerreira-MDF <theusferreiraode@icloud.com>
2024-10-22 20:37:35 -0300

    Delete src directory

--- src/main/java/com/projeto/sistema/repositorios/ProdutosRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.sistema.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.sistema.modelos.Produtos;
-
-public interface ProdutosRepositorio extends JpaRepository<Produtos, Long> {
-
-}
